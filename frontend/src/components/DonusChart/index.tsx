88ea81a1c021b6e14d199c887a98d9789e730519
Higor Souza <higor.andrade@hotmail.com>
2021-05-07 12:33:01 -0300

    Donutchart integration

--- frontend/src/components/DonusChart/index.tsx
+++ frontend/src/components/DonusChart/index.tsx
@@ -1,5 +1,6 @@
 
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -11,19 +12,19 @@ type ChartData = {
 
 const DonusChart = () => {
 
-    // FORMA ERRADA
-    let chartData: ChartData = { labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySeries };
-            console.log(chartData)
-        });
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, [])
+
     /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088, 140232],
         labels: ['Barry Allen', 'Paulo', 'Marcos', 'Pedro', 'João', 'Thiago']
