54389094a0df41d3c9175d370f63461b49b6f4a1
Higor Souza <higor.andrade@hotmail.com>
2021-05-07 13:10:46 -0300

    BarChart integration

--- frontend/src/components/DonusChart/index.tsx
+++ frontend/src/components/DonusChart/index.tsx
@@ -25,11 +25,6 @@ const DonusChart = () => {
             });
     }, [])
 
-    /*const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088, 140232],
-        labels: ['Barry Allen', 'Paulo', 'Marcos', 'Pedro', 'João', 'Thiago']
-    }*/
-
     const options = {
         legend: {
             show: true
